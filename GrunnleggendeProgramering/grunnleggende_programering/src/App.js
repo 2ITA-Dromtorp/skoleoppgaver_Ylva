bbf54d3106bff614f8d0cafb97311f211fdb1092
Ylva Aurora Dahl <ylvaauroradahl@Ylvas-MacBook-Pro.local>
2023-09-26 15:39:47 +0200

    Stain saks papir spill

--- GrunnleggendeProgramering/grunnleggende_programering/src/App.js
+++ GrunnleggendeProgramering/grunnleggende_programering/src/App.js
@@ -3,7 +3,7 @@ import Oppgave1 from './Oppgave1';
 import Oppgave2 from './Oppgave2';
 import Klokke from './klokke';
 import Nedtelling from './Nedtelling';
-import Stein_saks_papir from './stein_saks_papir';
+import Spill from './stein_saks_papir';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
 
         
-      <Stein_saks_papir />
+      <Spill />
 
       </header>
     </div>
--- GrunnleggendeProgramering/grunnleggende_programering/src/stein_saks_papir.js
+++ GrunnleggendeProgramering/grunnleggende_programering/src/stein_saks_papir.js
@@ -1,24 +1,75 @@
 import { useState } from 'react';
-import Ukjent from './images/maskin_ukjent.png'; 
-import Stein from './images/maskin_stein.png'; 
-
+import maskin_ukjent from './images/maskin_ukjent.png'; 
+import maskin_stein from './images/maskin_stein.png'; 
+import maskin_saks from './images/maskin_saks.png'; 
+import maskin_papir from './images/maskin_papir.png'; 
+import spiller_stein from './images/spiller_stein.png'; 
+import spiller_saks from './images/spiller_saks.png'; 
+import spiller_papir from './images/spiller_papir.png'; 
 
 
 export default function Spill() {
-    alert ('click');
+
+
+    const [PlayerChoice, setPlayerChoice] = useState(maskin_ukjent)
+    const [MaskinChoice, setMaskinChoice] = useState(maskin_ukjent)
+
+
+    function changeToStein() {
+        setPlayerChoice(spiller_stein)
+
+    }
+    function changeToSaks() {
+        setPlayerChoice(spiller_saks)
+
+    }
+    function changeToPapir() {
+        setPlayerChoice(spiller_papir)
+
+    }
+
+/*
+    function changeToStein() {
+        setMaskinChoice(maskin_stein)
+
+    }
+    function changeToSaks() {
+        setMaskinChoice(maskin_saks)
+
+    }
+    function changeToPapir() {
+        setMaskinChoice(maskin_papir)
+
+    }
+
+
+
+    function maskinStatus() {
+
+
+    }
+*/
+
     return (
         
-        <div>
+        <div class="SSP spill">
 
             <h1> stein, saks, papir spill  </h1>
-            <img src={Ukjent} alt="Ukjent"></img>
+            <img src={maskin_ukjent} alt="Ukjent"></img>
             <h2> vs </h2>
-            <img src={Ukjent} alt="Ukjent"></img>
-            <button onClick={Spill}>Stein</button>
-            <button onClick={Spill}>Saks</button>
-            <button onClick={Spill}>Papir</button>
+            <img src={PlayerChoice} alt="Ukjent"></img>
+            <div class="knapp" >
+            <button onClick={changeToStein}>Stein</button>
+            <button onClick={changeToSaks}>Saks</button>
+            <button onClick={changeToPapir}>Papir</button>
 
+            </div>
+            
+            <div class="SpillKnapp">
             <button onClick={Spill}>Spill</button>
+
+            </div>
+            
             
         </div>
     )
