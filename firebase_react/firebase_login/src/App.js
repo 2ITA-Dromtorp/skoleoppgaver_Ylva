6405e51892ed5da9722820be794df9156e8d9253
Ylva Aurora Dahl <ylvaauroradahl@Ylvas-MacBook-Pro.local>
2024-01-24 13:57:57 +0100

    bare hyggelig

--- firebase_react/firebase_login/src/App.js
+++ firebase_react/firebase_login/src/App.js
@@ -4,19 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <head>
+        <header className="App-header">
           <div className="nav">
 
           <h1> her blir login side </h1>
           </div>
-        </head>
-        <body> 
+        </header>
+      <body> 
         <div className="login">
           <h1> login side? </h1>
+          <p> brukernavn </p>
+          <input type="text" className="login-input"/>
+          <p> passord </p>
+          <input type="text" className="login-input"/>
+            <div id='login-knapp'> 
+                    <button className='logg'> logg inn nå </button>
+            </div>
         </div>
+
       </body>
-      </header>
      
     </div>
   );
